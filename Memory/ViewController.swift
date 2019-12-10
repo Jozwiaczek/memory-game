148686a4c004fd72b3a70456c8876efe440bd0bb
Jakub <kubencki@gmail.com>
2019-12-10 21:22:51 +0100

    Initial Commit

---
+++ Memory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by Jakub Jóźwiak on 10/12/2019.
+//  Copyright © 2019 Jakub Jóźwiak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
